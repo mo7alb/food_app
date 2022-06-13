2a6ee8a7e7fda9e85cc054188f4c6df65c615ad0
mo7alb <mo7hammad.alb@gmail.com>
2022-06-13 23:38:05 +0400

    packages install & imported

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Cell, Section, TableView } from 'react-native-tableview-simple';
 
 export default function App() {
   return (
